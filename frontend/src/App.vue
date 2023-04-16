b625910a32cb7197c30c0327ecc7fed6ca60bff8
Eric Schoebel <wir13fjs@studserv.uni-leipzig.de>
2023-04-16 15:50:48 +0200

    v tabs instead of v-list

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,6 +2,7 @@
   <v-app id="inspire">
 
 
+    <!--Navigation-Drawer-Links-->
     <v-navigation-drawer
         v-model="drawer"
         app
@@ -9,12 +10,31 @@
         permanent
         theme="dark"
       >
-        <v-list nav>
-          <v-list-item prepend-icon="mdi-home" title="Startseite" value="Startseite"></v-list-item>
-          <v-list-item prepend-icon="mdi-chart-areaspline" title="Wertevergleich" value="Wertevergleich"></v-list-item>
-          <v-list-item prepend-icon="mdi-chart-scatter-plot" title="Clustering" value="Clustering"></v-list-item>
-          <v-list-item prepend-icon="mdi-magnify" title="Anomalieerkennung" value="Anomalieerkennung"></v-list-item>
-        </v-list>
+      <v-tabs
+          v-model="tab"
+          direction="vertical"
+          :hide-slider="false"
+        >
+          <v-tab to="/" value="zero" :prepend-icon="(tab === 'zero') ? 'mdi-home': 'mdi-home'" :color="(tab === 'zero') ? 'teal-darken-1': 'default'">
+            Startseite
+          </v-tab>
+
+          <v-tab to="/Wertevergleich" value="one" :prepend-icon="(tab === 'one') ? 'mdi-chart-areaspline': 'mdi-chart-areaspline'" :color="(tab === 'one') ? 'teal-darken-1': 'default'">
+            Wertevergleich
+          </v-tab>
+
+          <v-tab to="/Clustering" value="two" :prepend-icon="(tab === 'two') ? 'mdi-chart-scatter-plot': 'mdi-chart-scatter-plot'" :color="(tab === 'two') ? 'teal-darken-1': 'default'">
+            Clustering
+          </v-tab>
+
+          <v-tab to="/Anomalieerkennung" value="three" :prepend-icon="(tab === 'three') ? 'mdi-magnify': 'mdi-magnify'" :color="(tab === 'three') ? 'teal-darken-1': 'default'">
+            Anomalieerkennung
+          </v-tab>
+          <v-divider></v-divider>
+      
+        </v-tabs>
+
+
       </v-navigation-drawer>
 
 
@@ -25,7 +45,7 @@
     </v-app-bar>
 
     <v-main>
-      <router-view></router-view>
+      <RouterView/>
       
       <!--  -->
     </v-main>
@@ -33,7 +53,27 @@
 </template>
 
 <script>
-  export default {
-    data: () => ({ drawer: null }),
-  }
+import { useTheme } from 'vuetify';
+import '@mdi/font/css/materialdesignicons.css'
+
+export default {
+  data () {
+    return {
+      drawer: null,
+      tab: "zero",
+    }
+  },
+  setup () {
+    const theme = useTheme()
+    return {
+      theme,
+      toggleTheme: () => {
+        theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+
+      }
+    }
+  },
+}
+
 </script>
+
